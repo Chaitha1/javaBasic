17d31308fe980b22ad5345722c914ad54152c332
Chaithra1 <R20200581.CHAITHRA@cse.reva.edu.in.com>
2024-01-18 16:02:32 +0530

    First Commit

---
+++ src/javatnsif/Dowhilecondition.java
@@ -0,0 +1,17 @@
+package javatnsif;
+
+public class Dowhilecondition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+	    do {
+	      System.out.println(i); // print without checking the condition
+	      i++;
+	    }
+	    while (i < 5);
+	    
+	    
+	}
+
+}
---
+++ src/javatnsif/breakcondition.java
@@ -0,0 +1,17 @@
+package javatnsif;
+
+public class breakcondition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		while (i < 10) {
+		  System.out.println(i);
+		  i++;
+		  if (i == 4) {
+		    break;
+		  }
+		}
+	}
+
+}
