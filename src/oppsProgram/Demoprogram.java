52aca2ac0bae82f7ce171bd73ec169303587ed4b
Chaithra1 <R20200581.CHAITHRA@cse.reva.edu.in.com>
2024-01-19 21:37:19 +0530

    Second Commit

---
+++ src/oppsProgram/Demoprogram.java
@@ -0,0 +1,16 @@
+package oppsProgram;
+
+public class Demoprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+		ProgramClas obj=new ProgramClas();
+		obj.setAge(22);
+		obj.setGender("Female");
+		obj.setName("CHAITHRA G");
+		
+		System.out.println(obj);
+	}
+
+}
---
+++ src/oppsProgram/ProgramClas.java
@@ -0,0 +1,34 @@
+package oppsProgram;
+
+public class ProgramClas {
+
+	private String name;
+	private int age;
+	private String gender;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	@Override
+	public String toString() {
+		return "ProgramClas [name=" + name + ", age=" + age + ", gender=" + gender + "]";
+	}
+	
+	
+	
+
+}
