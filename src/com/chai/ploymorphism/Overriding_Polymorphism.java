611095873a0856e70c4e0dea4aabaefdc5c69d9a
Chaithra1 <R20200581.CHAITHRA@cse.reva.edu.in.com>
2024-01-31 11:52:07 +0530

    First Commit

---
+++ src/com/chai/ploymorphism/Overriding_Polymorphism.java
@@ -0,0 +1,26 @@
+package com.chai.ploymorphism;
+ class Rbi{
+	 int a=10;
+	 int b=20;
+	 
+	 void sum() {
+		 int c=a+b;
+	 }
+ }
+ 
+ class Sbi extends Rbi{
+	 int x=20;
+	 int y =30;
+	 
+	 void sum() {
+		 int z=x+y;
+	 }
+ }
+public class Overriding_Polymorphism {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
